5401891bce454ce10a8fb0bcee2e8759c8fec7cf
Sigi Koizar <sigii@live.at>
2020-11-10 09:46:41 +0100

    test

--- vog_test/test_funct.py
+++ vog_test/test_funct.py
@@ -5,8 +5,11 @@ svc = VogService('/home/sigi/Documents/fastAPI/data')
 #filename = "/home/sigi/Documents/fastAPI/data"
 
 print("Testing Search:")
-result = svc.species.search(name='India')
+#result = svc.species.search(name='India')
+result = svc.species.search(id=1032892)
 print(result)
+for val in result:
+    print(val)
 
 
 
