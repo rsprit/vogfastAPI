39c10eee11eb295bb776cbc9a4be9ba33f13b559
Sigi Koizar <sigii@live.at>
2020-11-11 14:19:40 +0100

    test for species search

--- vog_test/test_funct.py
+++ vog_test/test_funct.py
@@ -1,15 +1,14 @@
 from typing import Set
-from unittest.mock import MagicMock
 
 from vogdb.functionality import VogService, SpeciesService
 from vogdb.vogdb_api import Species
 import unittest
 
 svc = VogService('/home/sigi/Documents/fastAPI/data')
-#filename = "/home/sigi/Documents/fastAPI/data"
+# filename = "/home/sigi/Documents/fastAPI/data"
 
 print("Testing Search:")
-#both names have to be contained. OR functionality?
+# both names have to be contained. OR functionality?
 result = svc.species.search(name=['India', 'leaf'], phage=False)
 res2 = svc.species[1002724]
 # IDs are unique -> when asking for ids, everything else is ignored.
@@ -20,7 +19,8 @@ print(res3)
 for val in res3:
     print(val)
 
-#result = svc.species.search(ids=[1032892])
+
+# result = svc.species.search(ids=[1032892])
 # print(result)
 # for val in result:
 #     print(val)
@@ -29,14 +29,15 @@ for val in res3:
 class TestSpeciesMethods(unittest.TestCase):
 
     def test_species_id(self):
-        print("Testing species id:")
-        self.assertEqual(svc.species[1002724], Species(id=1002724, name='Shigella virus Shfl1', phage=True, source='NCBI Refseq'))
+        # test get single species by ID:
+        self.assertEqual(svc.species[1002724],
+                         Species(id=1002724, name='Shigella virus Shfl1', phage=True, source='NCBI Refseq'))
 
-        list1 = [Species(id=1002724, name='Shigella virus Shfl1', phage=True, source='NCBI Refseq'),
-                 Species(id=1000664, name='Chilli leaf curl Vellanad virus [India/Vellanad/2008]', phage=False,
-                         source='NCBI Refseq')]
+        # test search by ID:
         self.assertEqual(list(svc.species.search(ids=[1002724, 1000664])),
-                          list1)
+                         [Species(id=1002724, name='Shigella virus Shfl1', phage=True, source='NCBI Refseq'),
+                          Species(id=1000664, name='Chilli leaf curl Vellanad virus [India/Vellanad/2008]', phage=False,
+                                  source='NCBI Refseq')])
 
 
 if __name__ == '__main__':
