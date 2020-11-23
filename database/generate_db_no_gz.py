d3c29a049667c71c06e5b8775da4b37067caa91c
Sigi Koizar <sigii@live.at>
2020-11-23 15:00:18 +0100

    species table fix1

--- database/generate_db_no_gz.py
+++ database/generate_db_no_gz.py
@@ -27,87 +27,82 @@ else:
     # Connect the database if exists.
     engine.connect()
 
-#
-# #---------------------
-# # VOG_table generation
-# #----------------------
-#
-# df = pd.read_csv(data_path + "vog.annotations.tsv", sep='\t')
-# df = df.rename(columns={"#GroupName": "VOG_ID"})
-# vog_fun_desc = df.iloc[:, 4].str.split(" ").str[1:].str.join(" ") #trimming consensus functional description
-# df_selected = df.iloc[:, [0,1,2,3]]
-# df_selected.insert(4, "Consensus_func_description", vog_fun_desc)
-#
-# # create a table in the database
-# df_selected.to_sql(name='VOG_profile', con=engine,
-#           if_exists='replace', index=False, chunksize=1000)
-#
-# with engine.connect() as con:
-#     con.execute('ALTER TABLE `VOG_profile` ADD PRIMARY KEY (`VOG_ID`(767));') #add primary key
-#     con.execute('ALTER TABLE VOG_profile  MODIFY  VOG_ID char(30) NOT NULL; ') #convert text to char
-#     con.execute('ALTER TABLE VOG_profile  MODIFY  FunctionalCategory char(30) NOT NULL; ')
-#     con.execute('ALTER TABLE VOG_profile  MODIFY  Consensus_func_description char(100) NOT NULL; ')
-#     con.execute('CREATE UNIQUE INDEX VOG_profile_index ON VOG_profile (VOG_ID, FunctionalCategory);') # create index
-#     con.execute('CREATE INDEX VOG_profile_index2 ON VOG_profile (Consensus_func_description);')  # create index
-#
-# print('VOG_table successfuly created!')
+# ---------------------
+# VOG_table generation
+# ----------------------
 
-#
-# #---------------------
-# # Protein_table generation
-# #----------------------
-# # extract proteins for each VOG
-# protein_list_df = pd.read_csv(data_path + "vog.members.tsv", sep='\t').iloc[:, [0,4]]
-#
-# # subsetting (only for testing purposes)
-# protein_list_df = protein_list_df.loc[np.random.choice(protein_list_df.index, 100, replace=False)]
-#
-# protein_list_df = protein_list_df.rename(columns={"#GroupName": "VOG_ID", "ProteinIDs": "ProteinID"})
-#
-#
-# # assign each protein a vog
-# protein_list_df = (protein_list_df["ProteinID"].str.split(",").apply(lambda x: pd.Series(x))
-#                    .stack()
-#                    .reset_index(level=1, drop=True)
-#                    .to_frame("ProteinID")
-#                    .join(protein_list_df[["VOG_ID"]], how="left")
-#                    )
-#
-# # separate protein and taxonID into separate columns
-# protein_list_df["TaxonID"] = protein_list_df["ProteinID"].str.split(".").str[0]
-# protein_list_df["ProteinID"] = protein_list_df["ProteinID"].str.split(".").str[1:3].str.join(".")
-#
-# # assign each protein its corresponding species by using NCBI entrez API
-# tax_ids = protein_list_df["TaxonID"]
-#
-# Entrez.email = 'vinkopcelica27@gmail.com'  # Put your email here
-# handle = Entrez.efetch('taxonomy', id=tax_ids, rettype='xml')
-# response = Entrez.read(handle)
-#
-# species_names = [entry.get('ScientificName') for entry in response]
-#
-#
-# #add species to the protein table
-# protein_list_df.insert(3, "Species_name", species_names)
-#
-# #ToDo: use the same approach with Entrez to create protein names column
-#
-#
-# # create a protein table in the database
-# protein_list_df.to_sql(name='Protein_profile', con=engine,
-#           if_exists='replace', index=False, chunksize=1000)
-#
-# with engine.connect() as con:
-#     con.execute('ALTER TABLE `Protein_profile` ADD PRIMARY KEY (`ProteinID`(767));')
-#     con.execute('ALTER TABLE Protein_profile  MODIFY  ProteinID char(30) NOT NULL; ')
-#     con.execute('ALTER TABLE Protein_profile  MODIFY  TaxonID int(30) NOT NULL; ')
-#     con.execute('ALTER TABLE Protein_profile  MODIFY  VOG_ID char(30) NOT NULL; ')
-#     con.execute('ALTER TABLE Protein_profile  MODIFY  Species_name char(100) NOT NULL; ')
-#     con.execute('CREATE INDEX Protein_profile_by_species ON Protein_profile (Species_name);')
-#     con.execute('CREATE INDEX VOG_profile_index_by_protein ON Protein_profile (ProteinID);')
-#
-# print('Protein_profile table successfully created!')
-#
+df = pd.read_csv(data_path + "vog.annotations.tsv", sep='\t')
+df = df.rename(columns={"#GroupName": "VOG_ID"})
+vog_fun_desc = df.iloc[:, 4].str.split(" ").str[1:].str.join(" ")  # trimming consensus functional description
+df_selected = df.iloc[:, [0, 1, 2, 3]]
+df_selected.insert(4, "Consensus_func_description", vog_fun_desc)
+
+# create a table in the database
+df_selected.to_sql(name='VOG_profile', con=engine,
+                   if_exists='replace', index=False, chunksize=1000)
+
+with engine.connect() as con:
+    con.execute('ALTER TABLE `VOG_profile` ADD PRIMARY KEY (`VOG_ID`(767));')  # add primary key
+    con.execute('ALTER TABLE VOG_profile  MODIFY  VOG_ID char(30) NOT NULL; ')  # convert text to char
+    con.execute('ALTER TABLE VOG_profile  MODIFY  FunctionalCategory char(30) NOT NULL; ')
+    con.execute('ALTER TABLE VOG_profile  MODIFY  Consensus_func_description char(100) NOT NULL; ')
+    con.execute('CREATE UNIQUE INDEX VOG_profile_index ON VOG_profile (VOG_ID, FunctionalCategory);')  # create index
+    con.execute('CREATE INDEX VOG_profile_index2 ON VOG_profile (Consensus_func_description);')  # create index
+
+print('VOG_table successfuly created!')
+
+# ---------------------
+# Protein_table generation
+# ----------------------
+# extract proteins for each VOG
+protein_list_df = pd.read_csv(data_path + "vog.members.tsv", sep='\t').iloc[:, [0, 4]]
+
+# subsetting (only for testing purposes)
+protein_list_df = protein_list_df.loc[np.random.choice(protein_list_df.index, 100, replace=False)]
+
+protein_list_df = protein_list_df.rename(columns={"#GroupName": "VOG_ID", "ProteinIDs": "ProteinID"})
+
+# assign each protein a vog
+protein_list_df = (protein_list_df["ProteinID"].str.split(",").apply(lambda x: pd.Series(x))
+                   .stack()
+                   .reset_index(level=1, drop=True)
+                   .to_frame("ProteinID")
+                   .join(protein_list_df[["VOG_ID"]], how="left")
+                   )
+
+# separate protein and taxonID into separate columns
+protein_list_df["TaxonID"] = protein_list_df["ProteinID"].str.split(".").str[0]
+protein_list_df["ProteinID"] = protein_list_df["ProteinID"].str.split(".").str[1:3].str.join(".")
+
+# assign each protein its corresponding species by using NCBI entrez API
+tax_ids = protein_list_df["TaxonID"]
+
+Entrez.email = 'vinkopcelica27@gmail.com'  # Put your email here
+handle = Entrez.efetch('taxonomy', id=tax_ids, rettype='xml')
+response = Entrez.read(handle)
+
+species_names = [entry.get('ScientificName') for entry in response]
+
+# add species to the protein table
+protein_list_df.insert(3, "Species_name", species_names)
+
+# ToDo: use the same approach with Entrez to create protein names column
+
+
+# create a protein table in the database
+protein_list_df.to_sql(name='Protein_profile', con=engine,
+                       if_exists='replace', index=False, chunksize=1000)
+
+with engine.connect() as con:
+    con.execute('ALTER TABLE `Protein_profile` ADD PRIMARY KEY (`ProteinID`(767));')
+    con.execute('ALTER TABLE Protein_profile  MODIFY  ProteinID char(30) NOT NULL; ')
+    con.execute('ALTER TABLE Protein_profile  MODIFY  TaxonID int(30) NOT NULL; ')
+    con.execute('ALTER TABLE Protein_profile  MODIFY  VOG_ID char(30) NOT NULL; ')
+    con.execute('ALTER TABLE Protein_profile  MODIFY  Species_name char(100) NOT NULL; ')
+    con.execute('CREATE INDEX Protein_profile_by_species ON Protein_profile (Species_name);')
+    con.execute('CREATE INDEX VOG_profile_index_by_protein ON Protein_profile (ProteinID);')
+
+print('Protein_profile table successfully created!')
 
 # ToDo creating other tables, modifying the existing tables, optimizing the structure
 # ---------------------
@@ -118,7 +113,7 @@ species_list_df = pd.read_csv(data_path + "vog.species.list",
                               sep='\t',
                               header=0,
                               names=['SpeciesName', 'TaxonID', 'Phage', 'Source', 'Version']) \
-                      .assign(Phage=lambda p: p.Phage == 'phage')
+    .assign(Phage=lambda p: p.Phage == 'phage')
 print(species_list_df)
 
 # create a species table in the database
@@ -132,5 +127,6 @@ with engine.connect() as con:
     con.execute('ALTER TABLE Species_profile  MODIFY  Source char(100) NOT NULL; ')
     con.execute('ALTER TABLE Species_profile  MODIFY  Version int(255) NOT NULL; ')
 
+# ToDo add foreign key? create index?
 
 print('Species_profile table successfully created!')
