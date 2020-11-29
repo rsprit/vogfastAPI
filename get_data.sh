a0b30b777cb0fcb7e8030a78b40afc539b4c58b6
sigi-k <74101001+sigi-k@users.noreply.github.com>
2020-11-29 21:59:23 +0100

    Add files via upload

---
+++ get_data.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+VERSION=${1:-latest}
+TARGET=data
+
+# create target directory
+
+mkdir -p ${TARGET}
+
+# clean up left-overs
+
+cd ${TARGET}
+rm -rf *
+
+# fetch all files from source
+
+wget -nv -r -np -nH -nd http://fileshare.csb.univie.ac.at/vog/vog${VERSION}/
+
+# remove unneeded markup files
+
+rm index.html*
+
+# unzip tab separated files and FASTA files
+
+gunzip *.tsv.gz *.fa.gz
+
+# untar archives
+
+for f in faa hmm raw_algs; do
+	mkdir ${f}
+	tar -x -z -C $f -f vog.$f.tar.gz
+	rm vog.$f.tar.gz
+done
+
+
