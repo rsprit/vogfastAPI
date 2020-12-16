3aefd97f2baed7b7646c003754b72da86e5d3567
mf <bioformative@gmail.com>
2020-12-16 19:10:27 +0100

    add update_db.py use double quotes for docstrings correct typos

--- database/update_db.py
+++ database/update_db.py
@@ -16,9 +16,9 @@ class Monitor:
         self.last_data = None
         self.pause_between_requests = 10  # Minutes
 
-    def updateDB(self):
-        ''' Get the modification dates from fileshare and call generate_db function if the dates changed
-        Raises: TypeError if no previous data exist'''
+    def update(self):
+        """Get the modification dates from fileshare and call generate_db function if the dates changed
+        Raises: TypeError if no previous data exist"""
         result = requests.get(self.url).text
         data = pd.read_html(result)[0]["Last modified"].dropna()
         try:
@@ -32,9 +32,9 @@ class Monitor:
 
     @staticmethod  # Maybe not necessary, call self in the function instead
     def checkequality(dateA, dateB):
-        ''' Check if all items in the lists dateA and dateB are identical
+        """Check if all items in the lists dateA and dateB are identical
             Returns: bool
-            Raises: IndexError'''
+            Raises: IndexError"""
         dateA = list(dateA)  # Type safety - putting an error if not type == list probably better practice
         dateB = list(dateB)
         for i, item in enumerate(dateA):
@@ -47,9 +47,9 @@ class Monitor:
 
 
 def run(self):
-    ''' Call indefinetly the updateDB function and suspend execution for the given number of seconds'''
+    """Call indefinitely the updateDB function and suspend execution for the given number of seconds"""
     while True:
-        self.updateDB()
+        self.update()
         sleep(self.pause_between_requests * 60)
 
 
