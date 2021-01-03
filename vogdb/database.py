424646ace6da40c8da19e48111ec67fa27ef3cf8
mf <bioformative@gmail.com>
2021-01-03 23:38:14 +0100

    update

--- vogdb/database.py
+++ vogdb/database.py
@@ -10,13 +10,13 @@ Note: you might need to change the MYSQL login credentials
 username = "root"
 password = "password"
 server = "localhost"
-database = "VOGDB"
-SQLALCHEMY_DATABASE_URL = ("mysql+pymysql://{0}:{1}@{2}/{3}").format(username, password, server, database)
+database = "vogdb"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://{0}:{1}@{2}/{3}".format(username, password, server, database)
 
 # Create an engine object.
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
-#Each instance of the SessionLocal class will be a database session
+# Each instance of the SessionLocal class will be a database session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # returns a class. Later we will inherit from this class to create each of the database models or classes
