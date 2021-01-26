7e3f6602d5b8093a557a75ca58e47a9d202f8dba
mf <bioformative@gmail.com>
2021-01-26 23:33:19 +0100

    defined function for download of vog files from url

---
+++ vog_download.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from urllib.request import urlretrieve
+import requests
+import pandas as pd
+
+def download():
+    
+    url = 'http://fileshare.csb.univie.ac.at/vog/latest/'
+    data_path = "data/"
+    vog = requests.get(url)
+    vog = vog.text
+    df = pd.DataFrame(pd.read_html(vog)[0]['Name'].dropna())
+    file_list = list(df['Name'][1:])    
+    
+    for f in file_list:
+        print(f'Downloading {f} to {data_path}')
+        urlretrieve(url+f, data_path+f)    
+    
